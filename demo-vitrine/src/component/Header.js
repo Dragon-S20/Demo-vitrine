62d5c53a0541e2289e96e66185d53130317a3ad0
Dragon-S20 <promoetherjs.dragons20@gmail.com>
2021-10-06 14:50:04 -0400

    navbar

--- demo-vitrine/src/component/Header.js
+++ demo-vitrine/src/component/Header.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Menu from './Menu.js'
+import Navbar from './Navbar.js'
 import SearchIcon from '@mui/icons-material/Search';
 
 const FlexGrow = () => {
@@ -8,7 +8,7 @@ const FlexGrow = () => {
     <div style={{ width: '100%' }}>
       <Box sx={{ display: 'flex', p: 1, bgcolor: 'background.paper'}}>
       <Box 
-        sx={{ p: 1, flexGrow: 1, bgcolor: 'grey.300' }}><Menu /></Box>
+        sx={{ p: 1, flexGrow: 1, bgcolor: 'grey.300' }}><Navbar /></Box>
         <Box 
         sx={{ p: 1, flexGrow: 1, bgcolor: 'grey.300'}}>Mon site Vitrine</Box>
         <Box sx={{ p: 1, bgcolor: 'grey.300' }}><SearchIcon color="primary" fontSize="large" sx={{ fontSize: 40 }}/></Box>
