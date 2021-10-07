123eb03615b8b0eaa25b265b51576d1c0d02a8f6
Dragon-S20 <promoetherjs.dragons20@gmail.com>
2021-10-07 04:55:11 -0400

    Color header

--- demo-vitrine/src/component/Header.js
+++ demo-vitrine/src/component/Header.js
@@ -3,18 +3,18 @@ import Box from '@mui/material/Box';
 import Navbar from './Navbar.js'
 import SearchIcon from '@mui/icons-material/Search';
 
-const FlexGrow = () => {
+const ShowHeader= () => {
   return (
-    <div style={{ width: '100%' }}>
-      <Box sx={{ display: 'flex', p: 1, bgcolor: 'background.paper'}}>
+    <div style={{ width: '100%' }} >
+      <Box sx={{ display: 'flex', p: 3, bgcolor: '#bdbdbd'}}>
       <Box 
-        sx={{ p: 1, flexGrow: 1, bgcolor: 'grey.300' }}><Navbar /></Box>
+        sx={{ p: 2, flexGrow: 1 }}><Navbar /></Box>
         <Box 
-        sx={{ p: 1, flexGrow: 1, bgcolor: 'grey.300'}}>Mon site Vitrine</Box>
-        <Box sx={{ p: 1, bgcolor: 'grey.300' }}><SearchIcon color="primary" fontSize="large" sx={{ fontSize: 40 }}/></Box>
+        sx={{ p: 3, flexGrow: 1}}>Mon site Vitrine</Box>
+        <Box sx={{ p: 2}}><SearchIcon color="primary" fontSize="large" sx={{ fontSize: 40 }}/></Box>
       </Box>
     </div>
   );
 }
 
-export default FlexGrow
+export default ShowHeader
